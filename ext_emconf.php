c970b743947916d16f1956b5655eca70177b10e4
Manuel Munz <munz@comuno.net>
2022-08-14 17:00:30 +0200

    Release version 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '0.1.0',
+    'version' => '1.0.0',
     'autoload' => [
         'psr-4' => [
             'C1\\C1SvgViewHelpers\\' => 'Classes',
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     ],
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99'
+            'typo3' => '11.5.0-11.5.99'
         ],
         'conflicts' => [
         ],
