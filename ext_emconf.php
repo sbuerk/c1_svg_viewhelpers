ac2af2a0c3c25f06bdd49b0a54b89903b38f85d8
Manuel Munz <munz@comuno.net>
2023-03-17 15:39:00 +0100

    Raise version to v1.1.0 in ext_emconf, allow TYPO3 up to 12.4.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'comuno.net',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'autoload' => [
         'psr-4' => [
             'C1\\C1SvgViewHelpers\\' => 'Classes',
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     ],
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.1.99'
+            'typo3' => '11.5.0-12.4.99'
         ],
         'conflicts' => [
         ],
